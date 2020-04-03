f2d6b7c77473c3a84757679d18d45e0d8c197fdb
rajebdev <wijanarko.rajeb@gmail.com>
2020-04-03 22:31:18 +0700

    + Add Flipper Food\n + Add Food Menu Slider

--- app/src/main/java/com/rajebdev/kuymakan/HomeFragment.java
+++ app/src/main/java/com/rajebdev/kuymakan/HomeFragment.java
@@ -1,7 +1,56 @@
 package com.rajebdev.kuymakan;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Start Flipper Image
+        ViewFlipper v_flipper = view.findViewById(R.id.v_flipper);
+        int[] images = {R.drawable.slider1, R.drawable.slider2, R.drawable.slider3, R.drawable.slider4, R.drawable.slider5};
+
+        for (int image: images)
+        {
+            ImageView imageView = new ImageView(this.getContext());
+            imageView.setBackgroundResource(image);
+
+            v_flipper.addView(imageView);
+            v_flipper.setFlipInterval(7000);
+            v_flipper.setAutoStart(true);
+
+            v_flipper.setInAnimation(this.getContext(),android.R.anim.slide_in_left);
+            v_flipper.setOutAnimation(this.getContext(),android.R.anim.slide_out_right);
+        }
+        // End Flipper Image
+
+        // Start Food Slider
+        FragmentTransaction ft = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+        for (int i=0; i < 5; i++)
+        {
+            Fragment itemFoodSlider = new FoodSliderFragment();
+            ft.add(R.id.food_slider_container, itemFoodSlider);
+        }
+        ft.commit();
+        // End Food Slider
+        return view;
+    }
 
 }
--- app/src/main/java/com/rajebdev/kuymakan/MainActivity.java
+++ app/src/main/java/com/rajebdev/kuymakan/MainActivity.java
@@ -1,14 +1,53 @@
 package com.rajebdev.kuymakan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);// kita set default nya Home Fragment
+        loadFragment(new HomeFragment());
+        // initialisation BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        // Tambahkan listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.coupon_menu:
+                fragment = new CouponFragment();
+                break;
+            case R.id.info_menu:
+                fragment = new InfoFragment();
+                break;
+            case R.id.profile_menu:
+                fragment = new ProfileFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
